874e171faaaec06400855349eb429e29a0903539
hania <hania.rudy@gmail.com>
2017-04-04 14:40:11 +0200

    add option to remove items

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var app = angular.module("myShoppingList", []);
 app.controller("listCtrl", function($scope) {
     $scope.products = ["Milk", "Eggs", "Bananas"];
     $scope.addProduct = function () {
-        $scope.products.push($scope.addItem);
+        $scope.errortext = '';
+        if (!$scope.addItem) {return;}
+        if ($scope.products.indexOf($scope.addItem) == -1) {
+            $scope.products.push($scope.addItem);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+
+    }
+    $scope.removeProduct = function (item) {
+        $scope.errortext = "";
+        $scope.products.splice(item, 1);
     }
 });
