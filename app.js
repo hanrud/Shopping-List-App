4b3c4af377ed45cfceb47acf943b49f19a160dd2
hania <hania.rudy@gmail.com>
2017-04-05 10:23:30 +0200

    add option to add items by keypress

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var app = angular.module("myShoppingList", []);
-app.controller("listCtrl", function($scope) {
+app.controller("listCtrl", function ($scope) {
     $scope.products = ["Milk", "Eggs", "Bananas"];
     $scope.addProduct = function () {
         $scope.errortext = '';
-        if (!$scope.addItem) {return;}
+        if (!$scope.addItem) {
+            return;
+        }
         if ($scope.products.indexOf($scope.addItem) === -1) {
             $scope.products.push($scope.addItem);
             $scope.addItem = '';
@@ -11,6 +13,11 @@ app.controller("listCtrl", function($scope) {
             $scope.errortext = "This product is already on your list.";
         }
 
+    };
+    $scope.pressed = function ($event, item) {
+        if ($event.which === 13) {
+            $scope.addProduct(item);
+        }
     };
     $scope.removeProduct = function (item) {
         $scope.errortext = "";
